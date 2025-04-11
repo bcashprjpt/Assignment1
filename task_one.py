7aa2497be64619595befe26dda533a509e03b821
bcashprjpt <128732932+bcashprjpt@users.noreply.github.com>
2025-04-11 15:55:45 +0530

    Add files via upload

---
+++ task_one.py
@@ -0,0 +1,11 @@
+#Task 1: Perform Basic Mathematical Operations
+num1  = float(input("Enter a number: "))
+num2  = float(input("Enter a number: "))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print (num1 ,"+", num2 , "= ", add)
+print (num1 ,"-", num2 , "= ", sub)
+print (num1 ,"*", num2 , "= ", mul)
+print (num1 ,"/", num2 , "= ", div)
---
+++ task_two.py
@@ -0,0 +1,5 @@
+#Task 2: Create a Personalized Greeting
+first = input("Enter your first name: ")
+last = input("Enter last name: ")
+greeting = "hello "+first+" "+last+" how a you, welcome to the python programming"
+print(greeting) 
